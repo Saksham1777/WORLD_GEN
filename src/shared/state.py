114bad316a75cd02fb259d00430e05bd96922357
Saksham1777 <155737907+Saksham1777@users.noreply.github.com>
2025-08-11 18:27:29 +0530

    Added Memory function

--- src/shared/state.py
+++ src/shared/state.py
@@ -1,25 +1,33 @@
 """
 Shared state definitions and types used by all agents.
-Acts as "contract" that all componests must follow.
+Acts as a "contract" that all components must follow.
 """
 
 from typing import Dict, Any, List
 from langchain_core.messages import BaseMessage
 from typing_extensions import TypedDict
 
+class MemoryEntry(TypedDict):
+    prompt: str
+    responding_agent: str
+    response: str
+    timestamp: str
+
 class AgentState(TypedDict):
     """
     The central state object that flows through our agent system.
     """
-    messages : List[BaseMessage] # full convo history
+    messages : List[BaseMessage] # full conversation history
     selected_agent : str
     agent_reasoning : str
-    input_prompt : str # user input prompt 
+    input_prompt : str
+    thread_id: int  # unique identifier for the conversation thread
+    thread_memory: Dict[int, List[MemoryEntry]]
 
 class AgentConfig(TypedDict):
     """
     Config structure of each agent.
-    Defines agents capabilites and behaviour
+    Defines agents capabilities and behaviour
     """
     description: str # what agent does
     system_prompt : str # agent core instructions
